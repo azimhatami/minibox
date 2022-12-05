ad0b2180e02b8ec407a13857a99d1d1a50d973b4
azimhatami <azimhatami4@gmail.com>
2022-12-05 17:04:41 -0500

    Bash atucompletion

--- minibox/__init__.py
+++ minibox/__init__.py
@@ -1,4 +1,4 @@
 from .cli import Minibox
 
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
--- minibox/cli.py
+++ minibox/cli.py
@@ -2,4 +2,18 @@ import easycli
 
 
 class Minibox(easycli.Root):
-    pass
+    __completion__ = True
+    __arguments__ = [
+        easycli.Argument(
+            '-v', '--version',
+            action='store_true',
+            help='Show version'
+        ),
+    ]
+    def __call__(self, args):
+        if args.version:
+            from minibox import __version__
+            print(__version__)
+            return
+
+        return super().__call__(args)
