7ab3b0fa22ac2e37af1ffe72a14d9157b7087a20
azimhatami <azimhatami4@gmail.com>
2022-12-06 15:57:18 -0500

    Recursive show

--- minibox/cli.py
+++ minibox/cli.py
@@ -1,4 +1,24 @@
+import os
+from os import path
+
 import easycli
+
+
+def issystemfile(filename):
+    abspath_ = path.abspath(filename)
+    base = path.basename(abspath_)
+    return base.startswith('.')
+
+
+class Show(easycli.SubCommand):
+    __command__ = 'show'
+
+    def __call__(self, args):
+        for root, dirs, files in os.walk('.'):
+            if issystemfile(root):
+                continue
+            items = ' '.join([i for i in dirs + files if not issystemfile(i)])
+            print(f'{root}:\n{items}\n')
 
 
 class Minibox(easycli.Root):
@@ -9,7 +29,9 @@ class Minibox(easycli.Root):
             action='store_true',
             help='Show version'
         ),
+        Show,
     ]
+
     def __call__(self, args):
         if args.version:
             from minibox import __version__
